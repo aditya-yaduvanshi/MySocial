d15f741d673258a0175244f7d22e9f346eb0a70d
adityayaduvanshi0001 <adityayaduvanshi0001@gmail.com>
2021-06-20 22:51:10 +0530

    fb script update

--- script.js
+++ script.js
@@ -55,60 +55,36 @@ window.fbAsyncInit = function() {
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
     if(response.status === 'connected'){
-      FbLoginSuccess();
-    }
-    else {
-      FB.login((response)=>{
-        checkLoginState()
-      }, {scope: 'public_profile'})
+      fblogin();
     }
   });
 }
 
-function FbLoginSuccess(){
+function fbloginfailed(){
+  alert('Login failed!\nUser cancelled or did not authorised completely.')
+}
+
+function fblogin(){
   FB.login(function(response){
-    FB.getLoginStatus(function(response) {
-      if(response.status === 'connected'){
-        let Permitted = ['true', 'false', 'false', 'false', 'false']
-        FB.api('/me/permissions', function(response){
-          console.log('permissions' + response)
-          if(response.data[1].permission == 'picture'){
-            if(response.data[1].status == 'granted'){
-              Permitted[1] = 'true'
-            }
-          }
-          if(response.data[2].permission == 'name'){
-            if(response.data[2].status == 'granted'){
-              Permitted[2] = 'true'
-            }
-          }
-          if(response.data[3].permission == 'email'){
-            if(response.data[3].status == 'granted'){
-              Permitted[3] = 'true'
-            }
-          }
-          if(response.data[4].permission == 'phone'){
-            if(response.data[4].status == 'granted'){
-              Permitted[4] = 'true'
-            }
-          }
-        })
-        if(Permitted[0] && Permitted[1] && Permitted[2] && Permitted[3] && Permitted[4]){
-          CallFbApi()
-        }
-      }
-    })
-  }, {scope:'public_profile, picture, name, email, phone'})
+    if(response.authResponse){
+      CallFbApi();
+    }
+    else {
+      fbloginfailed();
+    }
+  },{scope: 'email'})
 }
 
 function CallFbApi(){
   FB.api('/me?fields=name,email,picture.type(large),phone', function(response){
+    console.log('response: ' + response)
     console.log("Email : " + response.email)
     console.log("Name : " + response.name)
     console.log("Picture : " + response.picture)
     console.log("Phone : " + response.phone)
 
-    RedirectToProfile(response.picture, response.name, response.email, logOut)
+    let userid = (response.email) ? response.email : response.phone
+    RedirectToProfile(response.picture, response.name, userid, logOut)
 
   })
 }
